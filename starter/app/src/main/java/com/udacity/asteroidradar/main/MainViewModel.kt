97f839c1839c751d34537a6f27e04282f1dc2fe1
Sergio Aragonés <sergioat7@gmail.com>
2021-06-17 20:34:31 +0200

    Displayed picture of the day

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,40 @@
 package com.udacity.asteroidradar.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.api.NasaApi
+import com.udacity.asteroidradar.database.AsteroidDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val asteroidDao = AsteroidDatabase.getInstance(application).asteroidDatabaseDao
+    private val _pictureOfDay = MutableLiveData<PictureOfDay?>()
+
+    val pictureOfDay: LiveData<PictureOfDay?>
+        get() = _pictureOfDay
+
+    init {
+        getPictureOfDay()
+    }
+
+    private fun getPictureOfDay() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+
+                val imageOfTheDay = NasaApi.retrofitService.getImageOfTheDay(Constants.API_KEY)
+                _pictureOfDay.postValue(
+                    when (imageOfTheDay.mediaType) {
+                        "image" -> imageOfTheDay
+                        else -> null
+                    }
+                )
+            } catch (e: Exception) {
+                _pictureOfDay.postValue(null)
+            }
+        }
+    }
 }
